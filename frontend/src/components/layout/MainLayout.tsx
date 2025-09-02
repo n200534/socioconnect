994e66ad79862a80a6f974c7573bd89e09d2ebcc
Akshay kumar Amavarapu <n200534@rguktn.ac.in>
2025-09-03 05:21:53 +0530

    Update search icon color in MainLayout component for improved visibility

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -153,7 +153,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
             
             <div className="flex-1 max-w-2xl mx-6">
               <div className="relative">
-                <MagnifyingGlassIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
+                <MagnifyingGlassIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-900" />
                 <input
                   type="text"
                   placeholder="Search SocioConnect..."
